f76d93db78c311d1a4b38759d4aa28235ff9f550
Den4562 <119700990+Den4562@users.noreply.github.com>
2023-09-23 19:49:04 +0300

    Revert "Create class MessageBox"
    This reverts commit 470408fc2102a8c4f39b23aa98c3c2ab00e15f50.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Runtime.InteropServices;
+
 namespace collaboration
 {
     /// <summary>
@@ -20,28 +20,9 @@ namespace collaboration
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
-
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        class MessageBoxBase
-        {
-            public void ShowMessageBox(string name, string email, string information)
-            {
-                // Створити повідомлення з інформацією користувача
-                string message = $"Ім'я: {name} \nEmail: {email}\n Дополнительная информаци: {information}";
-
-                // Відобразити MessageBox з інформацією про автора
-                MessageBox(IntPtr.Zero, message, "Про автора", 0x40);
-            }
-
-
-        }
-
     }
 }
